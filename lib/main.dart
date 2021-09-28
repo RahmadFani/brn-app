125c93153ca1e7c7367d819a9b4b51ef1523d4db
Sio <siojurnalispipin@hotmail.com>
2021-09-28 09:31:57 +0700

    Fix: initial main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:brn/presentation/app_widget.dart';
+import 'package:brn/presentation/screens/nfc/providers/nfc_provider.dart';
 import 'package:brn/presentation/screens/peta_induk/providers/peta_induk.dart';
-import 'package:brn/providers/nfc_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
