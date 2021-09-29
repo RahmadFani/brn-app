3f8ac229b9172356e31e0797d3e122495b6700cd
Supriyadi <inyongmobile@gmail.com>
2021-09-29 20:19:55 +0700

    Pasang firebase core

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:brn/presentation/app_widget.dart';
 import 'package:brn/presentation/screens/nfc/providers/nfc_provider.dart';
 import 'package:brn/presentation/screens/peta_induk/providers/peta_induk.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 // ignore: avoid_void_async
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MultiProvider(
     providers: [
       ChangeNotifierProvider(create: (_) => NFCProvider()),
